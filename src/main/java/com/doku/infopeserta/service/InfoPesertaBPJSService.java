f706438f87d19956da25eb8f2f8d1f3055929ad1
rizkifani25 <rizkifani.me@gmail.com>
2020-07-24 12:56:49 +0700

    fix condition 'if', delete unnecessary object

--- src/main/java/com/doku/infopeserta/service/InfoPesertaBPJSService.java
+++ src/main/java/com/doku/infopeserta/service/InfoPesertaBPJSService.java
@@ -1,7 +1,6 @@
 package com.doku.infopeserta.service;
 
 import com.doku.infopeserta.model.BundleModel;
-import com.doku.infopeserta.model.MetadataModel;
 import com.doku.infopeserta.model.dataautodebet.*;
 import com.doku.infopeserta.model.pesertabpjs.*;
 import com.doku.infopeserta.model.pesertaautodebet.*;
@@ -25,7 +24,7 @@ public class InfoPesertaBPJSService {
     DataAutoDebetRepository dataAutoDebetRepository;
 
     private final RestTemplate restTemplate;
-    private final Logger LOG = LoggerFactory.getLogger(getClass()); 
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
     public InfoPesertaBPJSService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
@@ -36,39 +35,35 @@ public class InfoPesertaBPJSService {
         GetInfoPesertaBPJSResponse pesertaBPJS = new GetInfoPesertaBPJSResponse();
         GetInfoPesertaAutodebetResponse pesertaAutodebet = new GetInfoPesertaAutodebetResponse();
         TambahDataAutoDebetResponse insertNewDataResponse = new TambahDataAutoDebetResponse();
-        MetadataModel metadata = new MetadataModel();
         BundleModel bundleResponse = new BundleModel();
-        
+
         // BPJS InfoPesertaBPJS
         String urlExternal = "http://localhost:9090/InfoPesertaBPJS/{noka}";
         // BPJS TambahDataAutoDebet
         String urlInsertData = "http://localhost:9090/TambahDataAutoDebet";
         // WS GetInfoPesertaAutoDebet
         String urlInternal = "http://localhost:9090/InfoPesertaAutoDebet/{noka}";
-        String errorcode;
-        
+
         // HIT to BPJS InfoPesertaBPJS
         LOG.info("===== START Hit to BPJS InfoPesertaBPJS =====");
         pesertaBPJS = this.restTemplate.getForObject(urlExternal, GetInfoPesertaBPJSResponse.class, noka);
         LOG.info(pesertaBPJS.toString());
         LOG.info("===== END Hit to BPJS InfoPesertaBPJS =====");
 
-        metadata = pesertaBPJS.getMetadata();
-        errorcode = metadata.getErrorcode();
-        
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        
+
         HttpEntity<String> infoPesertaAutoDebetRequest = new HttpEntity<String>("OK", headers);
-        
+
         // HIT to WS GetInfoPesertaAutoDebet depends on the response
         LOG.info("===== START Hit WS GetInfoPesertaAutodebet =====");
-        pesertaAutodebet = this.restTemplate.postForObject(urlInternal, infoPesertaAutoDebetRequest, GetInfoPesertaAutodebetResponse.class, noka);
-        
-        if (errorcode.equals("1")) {
+        pesertaAutodebet = this.restTemplate.postForObject(urlInternal, infoPesertaAutoDebetRequest,
+                GetInfoPesertaAutodebetResponse.class, noka);
+
+        if (pesertaBPJS.getMetadata().getErrorcode().equals("1")) {
             bundleResponse.setData(pesertaAutodebet.getMetadata(), pesertaAutodebet.getResponse());
             LOG.info(bundleResponse.toString());
-        }else{
+        } else {
             bundleResponse.setData(pesertaAutodebet.getMetadata(), pesertaAutodebet.getResponse());
             LOG.info(bundleResponse.toString());
             LOG.info("===== END Hit WS GetInfoPesertaAutodebet =====");
@@ -76,19 +71,18 @@ public class InfoPesertaBPJSService {
         }
 
         LOG.info("===== END Hit WS GetInfoPesertaAutodebet =====");
-        
+
         // HIT to WS InsertAutoDebet depends on the response
         LOG.info("===== START Hit WS InsertAutodebet =====");
         TambahDataAutoDebetModel insertNewData = new TambahDataAutoDebetModel();
         insertNewData.setData(noka, 1, "1111112222", "Rizki Saja", "0888888888898", "rizki.saja@gmail.com", 0);
 
-        HttpEntity<TambahDataAutoDebetModel> tambahDataAutoDebetRequest = new HttpEntity<TambahDataAutoDebetModel>(insertNewData, headers);
+        HttpEntity<TambahDataAutoDebetModel> tambahDataAutoDebetRequest = new HttpEntity<TambahDataAutoDebetModel>(
+                insertNewData, headers);
 
         insertNewDataResponse = this.restTemplate.postForObject(urlInsertData, tambahDataAutoDebetRequest, TambahDataAutoDebetResponse.class, noka);
-        metadata = insertNewDataResponse.getMetadata();
-        errorcode = metadata.getErrorcode();
-        
-        if (errorcode.equals("1")) {
+
+        if (insertNewDataResponse.getMetadata().getErrorcode().equals("1")) {
             dataAutoDebetRepository.insertNewDataAutoDebet(noka, 1, "1111112222", "Rizki Saja", "0888888888898", "rizki.saja@gmail.com", 0);
             bundleResponse.setData(insertNewDataResponse.getMetadata(), insertNewDataResponse.getResponse());
             LOG.info(bundleResponse.toString());
